cb83c90f893b46ea7d3ff8bbaf6df13ebda59e81
Paul <paul.doyle@dit.ie>
2014-11-13 15:18:05 +0000

    aws files

--- list-was-queue.py
+++ list-was-queue.py
@@ -9,7 +9,7 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AK47IAJ2BJXBF74JPNZKCQ', aws_secret_access_key='mJyTlfZ+ZnDp5oe1tief0KpSqlUg52pIh4Fz3bOd')
 
 rs = conn.get_all_queues()
 for q in rs:
